3056bda391812800c7c3a2ae219da74e326d7977
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-26 22:08:46 -0700

    complete

--- calculator/main.go
+++ calculator/main.go
@@ -3,28 +3,32 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("value 1: ")
-	string1, _ := bufio.NewReader(os.Stdin).ReadString('\r')
-	value1, err := strconv.ParseInt(string1, 10, 64)
+	string1, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	value1, err := strconv.ParseFloat(strings.TrimSpace(string1), 64)
 
 	if err != nil {
-		fmt.Println(err)
 		panic("Value 1 must be a number")
 	}
 
 	fmt.Print("Value 2: ")
-	string2, _ := bufio.NewReader(os.Stdin).ReadString('\r')
-	value2, err := strconv.ParseFloat(string2, 64)
+	string2, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	value2, err := strconv.ParseFloat(strings.TrimSpace(string2), 64)
 
 	if err != nil {
 		panic("Value 2 must be a number")
 	}
 
-	fmt.Println(string1, string2, value1, value2)
+	result := math.Round((value1+value2)*100) / 100
+
+	fmt.Printf("The sum of %s and %s is %s", strconv.FormatFloat(value1, 'f', -1, 64), strconv.FormatFloat(value2, 'f', -1, 64), strconv.FormatFloat(result, 'f', -1, 64))
 
 }
