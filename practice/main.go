ec185d6f48d5e913d5f1dfd938939d00c35fbc6a
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-28 16:14:54 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Structs")
+	poodle := Dog{"poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Println(poodle.Breed)
+	poodle.Weight = 9
+	fmt.Println(poodle.Weight)
+}
+
+// Dog is a struct
+type Dog struct {
+	Breed  string
+	Weight int
 }
