b5eed7b3a7f71800c657d139b5ee0e7594f5c9ff
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-28 15:10:11 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "red"
+	colors[1] = "yellow"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[1])
+
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+	fmt.Println(len(numbers))
 }
