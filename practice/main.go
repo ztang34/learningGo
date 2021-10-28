b608fbf56b81a481701de6fb7ea7337765e9c09c
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-27 21:42:56 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	p := &anInt
+	fmt.Println("Value of p: ", *p)
+
+	value1 := 42.13
+	pointer1 := &value1
+	fmt.Println("Value 1: ", *pointer1)
+
+	*pointer1 = 22.5
+	fmt.Println("Value 1 after change: ", value1)
 }
