e5e6e0481a338d4cfe2bc9dd904ba0cf493927ae
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-28 21:33:36 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,23 @@ import (
 
 func main() {
 	fmt.Println("Conditional logic")
+
+	theAnswer := 42
+	var result string
+
+	if theAnswer < 0 {
+		result = "less than zero"
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+
+	if x := -42; x < 0 {
+		result = "Less than zero"
+	} else {
+		result = "greater or equal to zero"
+	}
+
+	fmt.Println(result)
 }
