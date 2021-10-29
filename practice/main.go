dfc790be902a3c489602e02899670256aa87c422
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-28 21:39:32 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,17 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var result string
+
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "Sunday"
+	case 2:
+		result = "Monday"
+	default:
+		result = "some other day"
+	}
+
+	fmt.Println(result)
 }
