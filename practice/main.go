108a50fbca472d216253bf394790ea4fede36df1
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-28 22:08:35 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,23 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	value := 1
+	for value < 10 {
+		fmt.Println(value)
+		value++
+	}
+
 }
