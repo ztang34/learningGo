afb9e4ebb2120afbbb80f9bdbf4a1fb723334c93
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-30 21:21:38 -0700

    complete

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
 	fmt.Println("Files")
+
+	content := "hell from go!"
+	file, err := os.Create("./fromString.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("%v characters have been written to file\n", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	checkError(err)
+	fmt.Println(" Text read from file:", string(data))
+
 }
