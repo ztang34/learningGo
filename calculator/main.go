abcfd446db817339adc73f8e5b614b0b69ce009d
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-30 20:55:55 -0700

    complete

--- calculator/main.go
+++ calculator/main.go
@@ -12,24 +12,42 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Value 1: ")
+	fmt.Println("Value 1: ")
 	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
+	float1 := ParseNumber(input1)
 
-	fmt.Print("Value 2: ")
+	fmt.Println("Value 2: ")
 	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
+	float2 := ParseNumber(input2)
+
+	fmt.Println("Select an operation ( + - * /): ")
+	sign, _ := reader.ReadString('\n')
+	var result float64
+
+	switch strings.TrimSpace(sign) {
+	case "+":
+		result = float1 + float2
+	case "-":
+		result = float1 - float2
+	case "*":
+		result = float1 * float2
+	case "/":
+		result = float1 / float2
+	default:
+		fmt.Println("Please provide a valid sign")
 	}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+	result = math.Round(result*100) / 100
+	fmt.Printf("The result is %v", result)
 
+}
+
+func ParseNumber(numString string) float64 {
+	number, err := strconv.ParseFloat(strings.TrimSpace(numString), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Cannot parse number")
+	} else {
+		return number
+	}
 }
