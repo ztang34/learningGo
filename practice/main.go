0bb62c60299fa831e8fac4c6ea22693472cee685
Zhenguan Tang <zhenguantang@gmail.com>
2021-10-30 21:31:34 -0700

    complete

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("The response type is %T: ", resp)
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bytes))
+
 }
